c92f40d183b2fcf184d5da8e0f392d430264d949
hankang370 <hankang370@bupt.edu.cn>
2020-10-20 22:35:19 +0800

    Java基础复习--注释、运算符、字符串

--- src/com/nowconder/Main.java
+++ src/com/nowconder/Main.java
@@ -1,5 +1,9 @@
 package com.nowconder;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.*;
+
 /**
  * @Description: 叶神牛客知乎问答社区前导课--Java基础复习(注释、字符串、运算符、变量)
  * @Author: 小韩同学
@@ -83,12 +87,150 @@ public class Main {
         print(16, sb.toString());
 
     }
-    public static void main(String[] args) {
-        // print(1,"Hello World 1"); // 打印
-        // demoOperation(); // 运算符
-        demoString(); // 字符串
+
+    /**
+     * 控制流演示
+     */
+    public static void demoControlFlow(){
+        int a = 2;
+        int target = a == 2 ? 1 : 3;
+        print(1, target);
+        if (a == 2){
+            target = 1;
+        } else {
+            target = 3;
+        }
+        print(2, target);
+        String grade = "B";
+        switch (grade){
+            case "A":
+                print(3, ">80");
+                break; // switch遇到break结束
+            case "B":
+                print(4,"60-80");
+                break;
+            case "C":
+                print(5,"<60");
+                break;
+            default:
+                print(6,"未知");
+                break;
+        }
+        for (int i = 0; i < 4; ++i) {
+            if(i == 0){
+                continue;
+            }
+            print(7,i);
+            if(i == 2){
+                break;
+            }
+        }
+        int score = 60;
+        while (score <= 100){
+            print(8,score);
+            score += 20;
+        }
+    }
+
+    /**
+     * ArrayList演示
+     */
+    private static void demoList() {
+        List<String> strList = new ArrayList<String>(10);
+        for (int i = 0; i < 4; i++) {
+            strList.add(String.valueOf(i*i)); // int转String
+        }
+        print(1,strList);
+
+        List<String> stringListB = new ArrayList<String>();
+        for (int i = 0; i < 4; i++) {
+            stringListB.add(String.valueOf(i));
+        }
+        strList.addAll(stringListB);
+        print(2,strList);
+        strList.remove(0);
+        print(3,strList);
+        strList.remove(String.valueOf(1)); // 1的String对象
+        print(4,strList);
+        print(5,strList.get(1)); // 角标1的元素
+
+        Collections.reverse(strList); // 反过来
+        print(6,strList);
+
+        Collections.sort(strList); // 排序
+        print(7,strList);
+
+        Collections.sort(strList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1); // 降序 o2 > o1 结果>0 所以是降序
+            }
+        });
+        print(8,strList);
+
+        for(String obj : strList){ // 遍历
+            print(9,obj);
+        }
+
+        for (int i = 0; i < strList.size(); i++) {
+            print(10,strList.get(i));
+        }
+
+        int[] array = new int[]{1,2,3};
+        print(11,array[1]);
+
+    }
+
+    private static void demoMapTable() {
+        Map<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < 4; i++) {
+            map.put(String.valueOf(i), String.valueOf(i*i));
+        }
+        print(1,map);
+
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            print(2,entry.getKey() + " | " + entry.getValue());
+        }
+
+        print(3,map.values());
+        print(4,map.keySet());
+        print(5,map.get("3"));
+        print(6,map.containsKey("A"));
+        map.replace("3", "27");
+        print(7,map.get("3"));
+    }
+
+    private static void demoSet() {
+        Set<String> strSet = new HashSet<String>();
+        for (int i = 0; i < 3; i++) {
+            strSet.add(String.valueOf(i));
+            strSet.add(String.valueOf(i));
+            strSet.add(String.valueOf(i));
+        }
+        print(1,strSet);
+        strSet.remove(String.valueOf(1));
+        print(2,strSet);
+        print(3,strSet.contains(String.valueOf(1)));
+        print(4,strSet.isEmpty());
+        print(5,strSet.size());
+
+        strSet.addAll(Arrays.asList(new String[]{"A", "B", "C"}));
+        print(6, strSet);
+
+        for(String str : strSet){
+            print(7,str);
+        }
 
     }
 
 
+    public static void main(String[] args) {
+        // print(1,"Hello World 1"); // 打印
+        // demoOperation(); // 运算符
+        // demoString(); // 字符串
+        // demoControlFlow();
+        // demoList();
+        // demoMapTable();
+        demoSet();
+    }
 }
